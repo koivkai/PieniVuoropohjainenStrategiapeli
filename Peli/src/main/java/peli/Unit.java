375fc07ef7fd14be4cd8700c69cbdfdf03282967
koivkai <kaius.koivunen@gmail.com>
2015-01-22 21:18:38 +0200

    muokkaus

--- Peli/src/main/java/peli/Unit.java
+++ Peli/src/main/java/peli/Unit.java
@@ -11,8 +11,22 @@ public class Unit {
     private double HP;
     private Tile tile;
     String name;
+
+    public Unit(int MP, double Defence, double Attack, double HP, Tile tile, String name) {
+        this.MP = MP;
+        this.Defence = Defence;
+        this.Attack = Attack;
+        this.HP = HP;
+        this.tile = tile;
+        this.name = name;
+    }
+    
+    
     
     public void Attack(Unit unit) {
+        double effectiveDef = unit.getDefence() + unit.getTile().getDefence();
+        double damage = (1 - effectiveDef) * this.Attack;
+        unit.TakeDamage(damage);
         
     }
     public void Move() {
@@ -22,6 +36,32 @@ public class Unit {
     public void Capture() {
     
 }
+
+    public double getDefence() {
+        return Defence;
+    }
+
+    public Tile getTile() {
+        return tile;
+    }
+    
+    public void TakeDamage(double damage) {
+        if (this.HP - damage <= 0) {
+            this.Die();
+            
+        }
+        else {
+            this.HP= this.HP - damage;
+        }
+    }
+    
+    public void Die() {
+        
+    }
+    
+     
+    
+    
    
 
 } 
